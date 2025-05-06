0bcd0d238bc15fba770fc66f1e16531a82382a18
Matti Luukkainen <mluukkai@iki.fi>
2025-05-06 20:13:32 +0300

    tweaks

--- server/index.js
+++ server/index.js
@@ -6,21 +6,24 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-const sequelize = new Sequelize({
+const dbConf = {
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: 5432,
   dialect: 'postgres',
-  dialectOptions: process.env.SSL !== 'true' ? {} : {
+  dialectOptions: process.env.SSL !== 'enabled' ? {} : {
     ssl: {
       require: true,
       rejectUnauthorized: false
     }
   }
-});
+}
 
+const sequelize = new Sequelize(dbConf);
+
+console.log('DB_CONF', dbConf);
 
 class Counter extends Model {}
 
